78205e5901acdb0370cc8346efb821fe6762c448
Yan <yangomesmd@gmail.com>
2022-06-26 23:04:43 -0400

    primeiro aqui

---
+++ script.js
@@ -0,0 +1,6 @@
+var divulgation = document.querySelector(".divulgation");
+var btnLer = document.querySelector(".btnLerMais");
+btnLer.addEventListener("click", () => {
+    btnLer.style.display = "none"
+    divulgation.style.display = "flex"
+})
